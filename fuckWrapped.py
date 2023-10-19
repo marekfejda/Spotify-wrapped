fe295421f589e67f7dfacb6f4faa59574d6b5ba4
Marek Fejda <marekfejda@gmail.com>
2023-10-19 20:05:47 +0200

    convert ms to human readable output

--- fuckWrapped.py
+++ fuckWrapped.py
@@ -5,7 +5,7 @@ import json
 file_prefix = "data\AccountData\MyData\\"
 file_names = ["StreamingHistory0.json", "StreamingHistory1.json", "StreamingHistory2.json", "StreamingHistory3.json", "StreamingHistory4.json"]
 
-total_ms_played_2023 = 0
+total_ms_played = 0
 
 for file_name in file_names:
     with open(file_prefix+file_name, 'r', encoding='utf-8') as file:
@@ -17,7 +17,12 @@ for file_name in file_names:
         
         if year == CURRENT_YEAR:
             ms_played = entry.get("msPlayed")
-            total_ms_played_2023 += ms_played
+            total_ms_played += ms_played
 
-# Print the total msPlayed in hours, minutes, and seconds
-print("Total msPlayed in 2023 from all files:", total_ms_played_2023)
+
+total_seconds = total_ms_played // 1000
+total_minutes = total_seconds // 60
+remaining_minutes = total_minutes % 60
+total_hours = total_minutes // 60
+
+print(f"Time spend listening to spotify [min]: {total_minutes} -> {total_hours} hours and {remaining_minutes} minutes")
